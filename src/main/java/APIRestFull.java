1f6edb4cc9b08a79a15b1358de30f044a2c12e6e
luchkovsky <a.luchkovsky90@gmail.com>
2016-10-05 23:14:29 +0300

    add git ignore to project

--- src/main/java/APIRestFull.java
+++ src/main/java/APIRestFull.java
@@ -1,7 +1,18 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/*http://www.ontestautomation.com/how-not-to-test-restful-apis-with-selenium-webdriver/*/
 
 /**
  * Created by luchk on 05.10.2016.
  */
 public class APIRestFull {
+    public static WebDriver driver;
+    public static void main(String[] args) {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://api.zippopotam.us/us/90210");
+    }
 }
